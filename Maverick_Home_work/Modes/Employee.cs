a29609d00d1944e4743a920e7b3f3d80777f9c54
Henry Kuo <mhhenryss@gmail.com>
2019-12-02 00:08:11 +0800

    extract class

--- Maverick_Home_work/Modes/Employee.cs
+++ Maverick_Home_work/Modes/Employee.cs
@@ -4,23 +4,31 @@ namespace Maverick_Home_work.Modes
 {
     public abstract class Employee
     {
+        private Logger _logger = new Logger();
+        public int Id { get; set; }
+        public Logger Logger
+        {
+            get { return _logger; }
+        }
+
         public DateTime StartWorkingDate { get; set; }
         public DateTime Today { get; set; }
+
+        public abstract decimal GetYearlyBonus();
 
         protected decimal GetMonthlySalary()
         {
             var message = $"query monthly salary id:{Id}";
-            Info(message);
+            Logger.Info(message);
             return SalaryRepo.Get(this.Id);
         }
+    }
 
-        public static void Info(string message)
+    public class Logger
+    {
+        public void Info(string message)
         {
             DebugHelper.Info(message);
         }
-
-        public abstract decimal GetYearlyBonus();
-
-        public int Id { get; set; }
     }
 }
--- Maverick_Home_work/Modes/LessThanOneYearEmployee.cs
+++ Maverick_Home_work/Modes/LessThanOneYearEmployee.cs
@@ -6,15 +6,15 @@ namespace Maverick_Home_work.Modes
     {
         public override decimal GetYearlyBonus()
         {
-            Info("--get yearly bonus--");
+            Logger.Info("--get yearly bonus--");
             var salary = this.GetMonthlySalary();
-            Info($"id:{Id}, his monthly salary is:{salary}");
+            Logger.Info($"id:{Id}, his monthly salary is:{salary}");
             return Convert.ToDecimal(this.WorkingYear()) * salary;
         }
 
         private double WorkingYear()
         {
-            Info("--get working year--");
+            Logger.Info("--get working year--");
             var year = (Today - StartWorkingDate).TotalDays / 365;
             return year > 1 ? 1 : Math.Round(year, 2);
         }
